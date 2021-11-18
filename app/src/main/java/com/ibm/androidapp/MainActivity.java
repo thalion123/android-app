87d2302a32e757c4f5dfcfc9bcbd17eda2280ea5
thalion123 <57055257+thalion123@users.noreply.github.com>
2021-11-18 04:57:25 -0300

    Initial commit

---
+++ app/src/main/java/com/ibm/androidapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ibm.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupWebView((WebView) findViewById(R.id.webView));
+    }
+
+    protected void setupWebView(WebView webView) {
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+}
